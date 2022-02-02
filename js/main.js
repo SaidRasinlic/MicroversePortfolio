0b7b65ddc18a0336d7247961696ccc30259eae4a
Said Rasinlic <57558480+SaidRasinlic@users.noreply.github.com>
2022-02-02 09:39:34 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -9,12 +9,11 @@ const closeMobileMenu = () => {
 document
   .querySelector('.openMenu')
   .addEventListener('click', displayMobileMenu);
-closeMobileItems.forEach((element) =>
-  element.addEventListener('click', closeMobileMenu));
+closeMobileItems.forEach((element) => element.addEventListener('click', closeMobileMenu));
 
 const projectDetail = [{
   title: "Tonic",
-  exprience: ["CANOPY", "Back End Dev", "2015"],
+  exprience: ['CANOPY', 'Back End Dev', '2015'],
   desription: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
   popDescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
   technologies: ["html", "css", "javascript"],
