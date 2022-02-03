0f38ff9788b4cb024fb9fe1c9f20baee29dc335e
Said Rasinlic <57558480+SaidRasinlic@users.noreply.github.com>
2022-02-03 18:29:58 +0100

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,32 +1,33 @@
-window.onload = function() { //ensures the page is loaded before functions are executed.
- document.getElementById("clear-butt").onclick = clearStorage
+window.onload = function () {
+  document.getElementById("clear-butt").onclick = clearStorage
 }
 
-function clearStorage() {
- //clears the entire localStorage
- localStorage.clear()
+function clearStorage () {
+  localStorage.clear()
 }
 
 function updateStorage() {
- var name = localStorage.getItem('name');
- var email = localStorage.getItem('email');
- var text = localStorage.getItem('text');
+  var name = localStorage.getItem('name');
+  var email = localStorage.getItem('email');
+  var text = localStorage.getItem('text');
 
- document.getElementById('name').value = name;
- document.getElementById('email').value = email;
- document.getElementById('text').value = text;
+  document.getElementById('name').value = name;
+  document.getElementById('email').value = email;
+  document.getElementById('text').value = text;
 }
 
-function populateStorage() {
- localStorage.setItem('name', document.getElementById('name').value);
- localStorage.setItem('email', document.getElementById('email').value);
- localStorage.setItem('text', document.getElementById('text').value);
+function populateStorage () {
+  localStorage.setItem('name', document.getElementById('name').value);
+  localStorage.setItem('email', document.getElementById('email').value);
+  localStorage.setItem('text', document.getElementById('text').value);
 
- updateStorage();
+  updateStorage();
 }
+
 name.onchange = populateStorage;
 email.onchange = populateStorage;
 text.onchange = populateStorage;
+
 if (!localStorage.getItem('name')) {
   populateStorage();
 } else {
