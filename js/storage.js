3f5bb3c9cb93ee021876749cd31cb7908b357004
Said Rasinlic <57558480+SaidRasinlic@users.noreply.github.com>
2022-02-03 18:14:32 +0100

    Local-Storage for form data

---
+++ js/storage.js
@@ -0,0 +1,37 @@
+if (!localStorage.getItem('name')) {
+ populateStorage();
+} else {
+ updateStorage();
+}
+
+window.onload = function() { //ensures the page is loaded before functions are executed.
+ document.getElementById("clear-butt").onclick = clearStorage
+}
+
+function clearStorage() {
+ //clears the entire localStorage
+ localStorage.clear()
+}
+
+function updateStorage() {
+ var name = localStorage.getItem('name');
+ var email = localStorage.getItem('email');
+ var text = localStorage.getItem('text');
+
+ document.getElementById('name').value = name;
+ document.getElementById('email').value = email;
+ document.getElementById('text').value = text;
+
+}
+
+function populateStorage() {
+ localStorage.setItem('name', document.getElementById('name').value);
+ localStorage.setItem('email', document.getElementById('email').value);
+ localStorage.setItem('text', document.getElementById('text').value);
+
+ updateStorage();
+}
+
+name.onchange = populateStorage;
+email.onchange = populateStorage;
+text.onchange = populateStorage;
