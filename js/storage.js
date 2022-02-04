fe94b78f2f72ecc6c40bacb6289b8ae4bb889112
Said Rasinlic <57558480+SaidRasinlic@users.noreply.github.com>
2022-02-04 13:39:58 +0100

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,20 +1,18 @@
-let getName = document.querySelector('#name');
-let getEmail = document.querySelector('#email');
-let getText = document.querySelector('#text');
+const getName = document.querySelector('#name');
+const getEmail = document.querySelector('#email');
+const getText = document.querySelector('#text');
 
-getName.addEventListener('change', function() {
- localStorage.setItem('name', document.getElementById('name').value);
-})
+getName.addEventListener('change', function () {localStorage.setItem('name', document.getElementById('name').value);});
 
-getEmail.addEventListener('change', function() { localStorage.setItem('email', document.getElementById('email').value);})
+getEmail.addEventListener('change', function () { localStorage.setItem('email', document.getElementById('email').value);});
 
-getText.addEventListener('change', function() { localStorage.setItem('text', document.getElementById('text').value);})
+getText.addEventListener('change', function () { localStorage.setItem('text', document.getElementById('text').value);});
 
-let localName = localStorage.getItem('name');
+const localName = localStorage.getItem('name');
 document.getElementById('name').value = localName;
 
-let localEmail = localStorage.getItem('email');
+const localEmail = localStorage.getItem('email');
 document.getElementById('email').value = localEmail;
 
-let localText = localStorage.getItem('text');
+const localText = localStorage.getItem('text');
 document.getElementById('text').value = localText;
